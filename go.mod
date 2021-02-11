eb6b8983031339e2ef46ebf076fc5788111e3fb7
Carlos A. Ospina <carlosaospinac@gmail.com>
2021-02-10 22:27:54 -0500

    Initialized with a simple Telegram bot

---
+++ go.mod
@@ -0,0 +1,5 @@
+module t-bot
+
+go 1.15
+
+require github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-beta3 // indirect
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters"
+)
+
+func main() {
+	b, err := gotgbot.NewBot(os.Getenv("TOKEN"))
+
+	if err != nil {
+		panic("failed to create new bot: " + err.Error())
+	}
+
+	// Create updater and dispatcher.
+	updater := ext.NewUpdater(b, nil)
+	dispatcher := updater.Dispatcher
+
+	// Add echo handler to reply to all messages.
+	dispatcher.AddHandler(handlers.NewMessage(filters.Text, echo))
+
+	// Start receiving updates.
+	err = updater.StartPolling(b, &ext.PollingOpts{Clean: true})
+	if err != nil {
+		panic("failed to start polling: " + err.Error())
+	}
+
+	fmt.Printf("%s has been started...\n", b.User.Username)
+
+	// Idle, to keep updates coming in, and avoid bot stopping.
+	updater.Idle()
+}
+
+// echo replies to a messages with its own contents
+func echo(ctx *ext.Context) error {
+	ctx.EffectiveMessage.Reply(ctx.Bot, ctx.EffectiveMessage.Text, nil)
+	return nil
+}
